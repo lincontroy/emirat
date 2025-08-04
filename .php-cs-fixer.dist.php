da2f5b2e776a33c9b2afacda7c8ab8862c9f0d97
lincontroy <lincolnmunene37@gmail.com>
2025-08-04 11:25:34 +0300

    updates

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,8 +1,14 @@
-// .php-cs-fixer.dist.php
 <?php
 
-return PhpCsFixer\Config::create()
-    ->setRules(['@PSR12' => true])
-    ->setFinder(
-        PhpCsFixer\Finder::create()->in(__DIR__)
-    );
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__);
+
+return (new Config())
+    ->setRules([
+        '@PSR12' => true,
+        'binary_operator_spaces' => ['default' => 'align_single_space_minimal'],
+    ])
+    ->setFinder($finder);
